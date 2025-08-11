4cd447d966921f6d24925bfcd27a082b253a4840
madhankumar2820 <madhankumar2820@gmail.com>
2025-08-11 13:46:48 +0530

    day 1 half completed

---
+++ Day 1/Javaone.java
@@ -0,0 +1,6 @@
+public class Javaone {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
---
+++ Day 1/Javathree.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public class Javathree {
+    public static void main(String[] args) {
+        int a= new Scanner(System.in).nextInt();
+        System.err.println(a);
+        
+    }
+}
